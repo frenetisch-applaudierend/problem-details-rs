916dc607709e88ad51a7ea322cb37af2474a3e1c
Markus Gasser <markus.gasser@mailbox.org>
2023-10-08 00:27:54 +0200

    More documentation and start with extensions

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
+//! RFC 9457 / RFC 7807 problem details for Rust applications.
+//!
+//! This crate implements a simple struct that can be used to
+//! represent a problem details object as defined in RFC 9457
+//! (which obsoletes RFC 7807).
+//!
+//! ## Features
+//!
+//! - `serde`: Enables serde support for the `ProblemDetails` struct.
+
+mod extensions;
 mod problem_details;
 mod problem_type;
 
+pub use extensions::Extensions;
 pub use problem_details::ProblemDetails;
 pub use problem_type::ProblemType;
 
 // Serde related extensions for http
 #[cfg(feature = "serde")]
 mod serde;
-
-// Re-export often used http structs
-pub use http::StatusCode;
-pub use http::Uri;
--- src/problem_details.rs
+++ src/problem_details.rs
@@ -1,6 +1,6 @@
 use http::{StatusCode, Uri};
 
-use crate::ProblemType;
+use crate::{Extensions, ProblemType};
 
 /// A RFC 9457 / RFC 7807 problem details object.
 ///
@@ -8,44 +8,47 @@ use crate::ProblemType;
 ///
 /// You can create a new problem details from a given
 /// status code using [`ProblemDetails::from_status_code`].
-/// 
+///
 /// This will set the `status` field to the given status code,
 /// the `title` field to the canonical reason phrase of the status code,
 /// and the `type` field to none, which is equivalen to `about:blank`.
-/// 
+///
 /// ```rust
-/// use problem_details::{ProblemDetails, StatusCode};
-/// 
+/// use http::StatusCode;
+/// use problem_details::ProblemDetails;
+///
 /// let details = ProblemDetails::from_status_code(StatusCode::NOT_FOUND);
-/// 
+///
 /// assert_eq!(details.status, Some(StatusCode::NOT_FOUND));
 /// assert_eq!(details.title, Some("Not Found".to_string()));
 /// assert_eq!(details.r#type.unwrap_or_default(), problem_details::ProblemType::default());
 /// ```
-/// 
+///
 /// You can then use the builder pattern to add additional fields.
-/// 
+///
 /// ```rust
-/// use problem_details::{ProblemDetails, StatusCode, Uri};
-/// 
+/// use http::{StatusCode, Uri};
+/// use problem_details::ProblemDetails;
+///
 /// let details = ProblemDetails::from_status_code(StatusCode::NOT_FOUND)
 ///    .with_type(Uri::from_static("example:type"))
 ///    .with_title("There is something wrong");
-/// 
+///
 /// assert_eq!(details.status, Some(StatusCode::NOT_FOUND));
 /// assert_eq!(details.title, Some("There is something wrong".to_string()));
 /// assert_eq!(details.r#type.unwrap_or_default(), Uri::from_static("example:type").into());
 /// ```
-/// 
+///
 /// You can also create a new problem details object using [`ProblemDetails::new`].
-/// 
+///
 /// ```rust
-/// use problem_details::{ProblemDetails, Uri};
-/// 
+/// use http::Uri;
+/// use problem_details::ProblemDetails;
+///
 /// let details = ProblemDetails::new()
 ///   .with_type(Uri::from_static("example:type"))
 ///   .with_title("There is something wrong");
-/// 
+///
 /// assert_eq!(details.status, None);
 /// assert_eq!(details.title, Some("There is something wrong".to_string()));
 /// assert_eq!(details.r#type.unwrap_or_default(), Uri::from_static("example:type").into());
@@ -54,62 +57,72 @@ use crate::ProblemType;
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct ProblemDetails {
     /// An optional uri describing the problem type.
-    /// 
+    ///
     /// See [https://www.rfc-editor.org/rfc/rfc9457.html#name-type]() for more information.
+    #[cfg_attr(feature = "serde", serde(default))]
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub r#type: Option<ProblemType>,
 
     /// An optional status code for this problem.
-    /// 
+    ///
     /// See [https://www.rfc-editor.org/rfc/rfc9457.html#name-status]() for more information.
+    #[cfg_attr(feature = "serde", serde(default))]
     #[cfg_attr(feature = "serde", serde(with = "crate::serde::status::opt"))]
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub status: Option<StatusCode>,
 
     /// An optional human-readable title for this problem.
-    /// 
+    ///
     /// See [https://www.rfc-editor.org/rfc/rfc9457.html#name-title]() for more information.
+    #[cfg_attr(feature = "serde", serde(default))]
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub title: Option<String>,
 
     /// An optional human-readable description of this problem.
-    /// 
+    ///
     /// See [https://www.rfc-editor.org/rfc/rfc9457.html#name-detail]() for more information.
+    #[cfg_attr(feature = "serde", serde(default))]
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub detail: Option<String>,
 
     /// An optional uri identifying the specific instance of this problem.
-    /// 
+    ///
     /// See [https://www.rfc-editor.org/rfc/rfc9457.html#name-instance]() for more information.
+    #[cfg_attr(feature = "serde", serde(default))]
     #[cfg_attr(feature = "serde", serde(with = "crate::serde::uri::opt"))]
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub instance: Option<Uri>,
+
+    #[cfg_attr(feature = "serde", serde(flatten))]
+    pub extensions: Extensions,
 }
 
 impl ProblemDetails {
     /// Creates a new empty problem details object.
     pub fn new() -> Self {
-        ProblemDetails {
+        Self {
             r#type: None,
             status: None,
             title: None,
             detail: None,
             instance: None,
+            extensions: Extensions::new(),
         }
     }
 
     /// Creates a new problem details object from a given status code.
-    /// 
+    ///
     /// This will set the `status` field to the given status code,
     /// the `title` field to the canonical reason phrase of the status code,
     /// and the `type` field to none, which is equivalent to `about:blank`.
     pub fn from_status_code(status: StatusCode) -> Self {
-        ProblemDetails {
+        Self {
             r#type: None,
             status: Some(status),
             title: status.canonical_reason().map(ToOwned::to_owned),
             detail: None,
             instance: None,
+            extensions: Extensions::new(),
         }
     }
 
@@ -147,6 +160,7 @@ impl ProblemDetails {
 #[cfg(test)]
 mod tests {
     use http::{StatusCode, Uri};
+    use serde_json::json;
 
     use crate::{ProblemDetails, ProblemType};
 
@@ -190,7 +204,7 @@ mod tests {
 
         let serialized = serde_json::to_value(empty).unwrap();
 
-        let expected = serde_json::json!({});
+        let expected = json!({});
 
         assert_eq!(expected, serialized);
     }
@@ -207,7 +221,7 @@ mod tests {
 
         let serialized = serde_json::to_value(details).unwrap();
 
-        let expected = serde_json::json!({
+        let expected = json!({
             "type": "test:type",
             "status": 500,
             "title": "Test Title",
@@ -216,5 +230,40 @@ mod tests {
         });
 
         assert_eq!(expected, serialized);
+    }
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn deserialize_empty() {
+        let empty = json!({});
+
+        let deserialized = serde_json::from_value(empty).unwrap();
+
+        let expected = ProblemDetails::new();
+
+        assert_eq!(expected, deserialized);
+    }
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn deserialize_filled() {
+        let filled = json!({
+            "type": "test:type",
+            "status": 500,
+            "title": "Test Title",
+            "detail": "Test Detail",
+            "instance": "test:instance"
+        });
+
+        let deserialized = serde_json::from_value(filled).unwrap();
+
+        let expected = ProblemDetails::new()
+            .with_type(Uri::from_static("test:type"))
+            .with_status(StatusCode::INTERNAL_SERVER_ERROR)
+            .with_title("Test Title")
+            .with_detail("Test Detail")
+            .with_instance(Uri::from_static("test:instance"));
+
+        assert_eq!(expected, deserialized);
     }
 }
