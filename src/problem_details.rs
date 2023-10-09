8e42ab90ec5f83c384dd31802c6fdf55f5138374
Markus Gasser <markus.gasser@mailbox.org>
2023-10-10 00:02:00 +0200

    Improve documentation

--- src/problem_details.rs
+++ src/problem_details.rs
@@ -2,6 +2,9 @@ use http::{StatusCode, Uri};
 
 use crate::ProblemType;
 
+#[cfg(test)]
+mod tests;
+
 /// A RFC 9457 / RFC 7807 problem details object.
 ///
 /// # Creating problem details
@@ -9,9 +12,9 @@ use crate::ProblemType;
 /// You can create a new problem details from a given
 /// status code using [`ProblemDetails::from_status_code`].
 ///
-/// This will set the `status` field to the given status code,
-/// the `title` field to the canonical reason phrase of the status code,
-/// and the `type` field to none, which is equivalen to `about:blank`.
+/// This will set the [`status`](ProblemDetails::status) field to the given status code,
+/// the [`title`](ProblemDetails::title) field to the canonical reason phrase of the status code,
+/// and the [`type`](ProblemDetails::type) field to none, which is equivalent to `about:blank`.
 ///
 /// ```rust
 /// use http::StatusCode;
@@ -52,6 +55,50 @@ use crate::ProblemType;
 /// assert_eq!(details.status, None);
 /// assert_eq!(details.title, Some("There is something wrong".to_string()));
 /// assert_eq!(details.r#type.unwrap_or_default(), Uri::from_static("example:type").into());
+/// ```
+/// # Extensions
+///
+/// To add extensions, you need to define a struct that holds the extension
+/// fields, and use this struct as the generic parameter for [`ProblemDetails<Ext>`].
+/// Using [`with_extensions`](ProblemDetails::with_extensions), the type is adjusted
+/// automatically for you.
+///
+/// Extension fields are flattened into the problem details object when serialized.
+///
+/// ```rust
+/// use problem_details::ProblemDetails;
+///
+/// struct MyExt {
+///     foo: String,
+///     bar: u32,
+/// }
+///
+/// let details = ProblemDetails::new()
+///     .with_extensions(MyExt {
+///         foo: "Hello".to_string(),
+///         bar: 42,
+///     });
+///
+/// // details is of type ProblemDetails<MyExt>
+/// let typecheck: ProblemDetails<MyExt> = details;
+/// ```
+///
+/// If you need dynamic extensions, you can use a [`HashMap`](std::collections::HashMap)
+/// as extensions object.
+///
+/// ```rust
+/// use std::collections::HashMap;
+/// use problem_details::ProblemDetails;
+///
+/// let mut extensions = HashMap::<String, serde_json::Value>::new();
+/// extensions.insert("foo".to_string(), serde_json::json!("Hello"));
+/// extensions.insert("bar".to_string(), serde_json::json!(42));
+///
+/// let details = ProblemDetails::new()
+///    .with_extensions(extensions);
+///
+/// // details is of type ProblemDetails<HashMap<String, serde_json::Value>>
+/// let typecheck: ProblemDetails<HashMap<String, serde_json::Value>> = details;
 /// ```
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -212,244 +259,3 @@ impl<Ext> std::fmt::Display for ProblemDetails<Ext> {
 }
 
 impl<Ext> std::error::Error for ProblemDetails<Ext> where Ext: std::fmt::Debug {}
-
-#[cfg(test)]
-mod tests {
-    use http::{StatusCode, Uri};
-    use serde_json::json;
-
-    use crate::{ProblemDetails, ProblemType};
-
-    #[test]
-    fn from_status() {
-        let details = ProblemDetails::from_status_code(StatusCode::NOT_FOUND);
-
-        assert_eq!(
-            details.r#type.unwrap_or_default(),
-            ProblemType::from(Uri::from_static("about:blank"))
-        );
-        assert_eq!(details.status, Some(StatusCode::NOT_FOUND));
-        assert_eq!(details.title, Some("Not Found".to_string()));
-        assert_eq!(details.detail, None);
-        assert_eq!(details.instance, None);
-        assert_eq!(details.extensions, ());
-    }
-
-    #[test]
-    fn fully_configured() {
-        #[derive(Debug, PartialEq, Eq)]
-        struct Extensions {
-            foo: String,
-            bar: u32,
-        }
-
-        let details = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_status(StatusCode::INTERNAL_SERVER_ERROR)
-            .with_title("Test Title")
-            .with_detail("Test Detail")
-            .with_instance(Uri::from_static("test:instance"))
-            .with_extensions(Extensions {
-                foo: "Foo".to_string(),
-                bar: 42,
-            });
-
-        assert_eq!(
-            details.r#type,
-            Some(ProblemType::from(Uri::from_static("test:type")))
-        );
-        assert_eq!(details.status, Some(StatusCode::INTERNAL_SERVER_ERROR));
-        assert_eq!(details.title, Some("Test Title".to_string()));
-        assert_eq!(details.detail, Some("Test Detail".to_string()));
-        assert_eq!(details.instance, Some(Uri::from_static("test:instance")));
-        assert_eq!(
-            details.extensions,
-            Extensions {
-                foo: "Foo".to_string(),
-                bar: 42
-            }
-        );
-    }
-
-    #[test]
-    fn to_string() {
-        let empty = ProblemDetails::new();
-
-        let type_only = ProblemDetails::new().with_type(Uri::from_static("test:type"));
-        let status_only = ProblemDetails::new().with_status(StatusCode::NOT_FOUND);
-        let title_only = ProblemDetails::new().with_title("Test Title");
-        let detail_only = ProblemDetails::new().with_detail("Test Detail");
-
-        let type_status = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_status(StatusCode::NOT_FOUND);
-        let type_title = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_title("Test Title");
-        let type_detail = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_detail("Test Detail");
-        let status_title = ProblemDetails::new()
-            .with_status(StatusCode::NOT_FOUND)
-            .with_title("Test Title");
-        let status_detail = ProblemDetails::new()
-            .with_status(StatusCode::NOT_FOUND)
-            .with_detail("Test Detail");
-        let title_detail = ProblemDetails::new()
-            .with_title("Test Title")
-            .with_detail("Test Detail");
-
-        let type_status_title = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_status(StatusCode::NOT_FOUND)
-            .with_title("Test Title");
-        let type_status_detail = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_status(StatusCode::NOT_FOUND)
-            .with_detail("Test Detail");
-        let type_title_detail = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_title("Test Title")
-            .with_detail("Test Detail");
-        let status_title_detail = ProblemDetails::new()
-            .with_status(StatusCode::NOT_FOUND)
-            .with_title("Test Title")
-            .with_detail("Test Detail");
-
-        let full = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_status(StatusCode::NOT_FOUND)
-            .with_title("Test Title")
-            .with_detail("Test Detail");
-
-        assert_eq!("[about:blank]", empty.to_string());
-
-        assert_eq!("[test:type]", type_only.to_string());
-        assert_eq!("[about:blank 404] Not Found", status_only.to_string());
-        assert_eq!("[about:blank] Test Title", title_only.to_string());
-        assert_eq!("[about:blank] Test Detail", detail_only.to_string());
-
-        assert_eq!("[test:type 404] Not Found", type_status.to_string());
-        assert_eq!("[test:type] Test Title", type_title.to_string());
-        assert_eq!("[test:type] Test Detail", type_detail.to_string());
-        assert_eq!("[about:blank 404] Test Title", status_title.to_string());
-        assert_eq!(
-            "[about:blank 404] Not Found: Test Detail",
-            status_detail.to_string()
-        );
-        assert_eq!(
-            "[about:blank] Test Title: Test Detail",
-            title_detail.to_string()
-        );
-
-        assert_eq!("[test:type 404] Test Title", type_status_title.to_string());
-        assert_eq!(
-            "[test:type 404] Not Found: Test Detail",
-            type_status_detail.to_string()
-        );
-        assert_eq!(
-            "[test:type] Test Title: Test Detail",
-            type_title_detail.to_string()
-        );
-        assert_eq!(
-            "[about:blank 404] Test Title: Test Detail",
-            status_title_detail.to_string()
-        );
-
-        assert_eq!("[test:type 404] Test Title: Test Detail", full.to_string());
-    }
-
-    #[cfg(feature = "serde")]
-    #[test]
-    fn serialize_empty() {
-        let empty = ProblemDetails::new();
-
-        let serialized = serde_json::to_value(empty).unwrap();
-
-        let expected = json!({});
-
-        assert_eq!(expected, serialized);
-    }
-
-    #[cfg(feature = "serde")]
-    #[test]
-    fn serialize_filled() {
-        #[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
-        struct Extensions {
-            foo: String,
-            bar: u32,
-        }
-
-        let details = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_status(StatusCode::INTERNAL_SERVER_ERROR)
-            .with_title("Test Title")
-            .with_detail("Test Detail")
-            .with_instance(Uri::from_static("test:instance"))
-            .with_extensions(Extensions {
-                foo: "Foo".to_string(),
-                bar: 42,
-            });
-
-        let serialized = serde_json::to_value(details).unwrap();
-
-        let expected = json!({
-            "type": "test:type",
-            "status": 500,
-            "title": "Test Title",
-            "detail": "Test Detail",
-            "instance": "test:instance",
-            "foo": "Foo",
-            "bar": 42
-        });
-
-        assert_eq!(expected, serialized);
-    }
-
-    #[cfg(feature = "serde")]
-    #[test]
-    fn deserialize_empty() {
-        let empty = json!({});
-
-        let deserialized = serde_json::from_value(empty).unwrap();
-
-        let expected = ProblemDetails::new();
-
-        assert_eq!(expected, deserialized);
-    }
-
-    #[cfg(feature = "serde")]
-    #[test]
-    fn deserialize_filled() {
-        #[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
-        struct Extensions {
-            foo: String,
-            bar: u32,
-        }
-
-        let filled = json!({
-            "type": "test:type",
-            "status": 500,
-            "title": "Test Title",
-            "detail": "Test Detail",
-            "instance": "test:instance",
-            "foo": "Foo",
-            "bar": 42
-        });
-
-        let deserialized: ProblemDetails<Extensions> = serde_json::from_value(filled).unwrap();
-
-        let expected = ProblemDetails::new()
-            .with_type(Uri::from_static("test:type"))
-            .with_status(StatusCode::INTERNAL_SERVER_ERROR)
-            .with_title("Test Title")
-            .with_detail("Test Detail")
-            .with_instance(Uri::from_static("test:instance"))
-            .with_extensions(Extensions {
-                foo: "Foo".to_string(),
-                bar: 42,
-            });
-
-        assert_eq!(expected, deserialized);
-    }
-}
---
+++ src/problem_details/tests.rs
@@ -0,0 +1,237 @@
+use http::{StatusCode, Uri};
+use serde_json::json;
+
+use crate::{ProblemDetails, ProblemType};
+
+#[test]
+fn from_status() {
+    let details = ProblemDetails::from_status_code(StatusCode::NOT_FOUND);
+
+    assert_eq!(
+        details.r#type.unwrap_or_default(),
+        ProblemType::from(Uri::from_static("about:blank"))
+    );
+    assert_eq!(details.status, Some(StatusCode::NOT_FOUND));
+    assert_eq!(details.title, Some("Not Found".to_string()));
+    assert_eq!(details.detail, None);
+    assert_eq!(details.instance, None);
+    assert_eq!(details.extensions, ());
+}
+
+#[test]
+fn fully_configured() {
+    #[derive(Debug, PartialEq, Eq)]
+    struct Extensions {
+        foo: String,
+        bar: u32,
+    }
+
+    let details = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_status(StatusCode::INTERNAL_SERVER_ERROR)
+        .with_title("Test Title")
+        .with_detail("Test Detail")
+        .with_instance(Uri::from_static("test:instance"))
+        .with_extensions(Extensions {
+            foo: "Foo".to_string(),
+            bar: 42,
+        });
+
+    assert_eq!(
+        details.r#type,
+        Some(ProblemType::from(Uri::from_static("test:type")))
+    );
+    assert_eq!(details.status, Some(StatusCode::INTERNAL_SERVER_ERROR));
+    assert_eq!(details.title, Some("Test Title".to_string()));
+    assert_eq!(details.detail, Some("Test Detail".to_string()));
+    assert_eq!(details.instance, Some(Uri::from_static("test:instance")));
+    assert_eq!(
+        details.extensions,
+        Extensions {
+            foo: "Foo".to_string(),
+            bar: 42
+        }
+    );
+}
+
+#[test]
+fn to_string() {
+    let empty = ProblemDetails::new();
+
+    let type_only = ProblemDetails::new().with_type(Uri::from_static("test:type"));
+    let status_only = ProblemDetails::new().with_status(StatusCode::NOT_FOUND);
+    let title_only = ProblemDetails::new().with_title("Test Title");
+    let detail_only = ProblemDetails::new().with_detail("Test Detail");
+
+    let type_status = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_status(StatusCode::NOT_FOUND);
+    let type_title = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_title("Test Title");
+    let type_detail = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_detail("Test Detail");
+    let status_title = ProblemDetails::new()
+        .with_status(StatusCode::NOT_FOUND)
+        .with_title("Test Title");
+    let status_detail = ProblemDetails::new()
+        .with_status(StatusCode::NOT_FOUND)
+        .with_detail("Test Detail");
+    let title_detail = ProblemDetails::new()
+        .with_title("Test Title")
+        .with_detail("Test Detail");
+
+    let type_status_title = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_status(StatusCode::NOT_FOUND)
+        .with_title("Test Title");
+    let type_status_detail = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_status(StatusCode::NOT_FOUND)
+        .with_detail("Test Detail");
+    let type_title_detail = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_title("Test Title")
+        .with_detail("Test Detail");
+    let status_title_detail = ProblemDetails::new()
+        .with_status(StatusCode::NOT_FOUND)
+        .with_title("Test Title")
+        .with_detail("Test Detail");
+
+    let full = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_status(StatusCode::NOT_FOUND)
+        .with_title("Test Title")
+        .with_detail("Test Detail");
+
+    assert_eq!("[about:blank]", empty.to_string());
+
+    assert_eq!("[test:type]", type_only.to_string());
+    assert_eq!("[about:blank 404] Not Found", status_only.to_string());
+    assert_eq!("[about:blank] Test Title", title_only.to_string());
+    assert_eq!("[about:blank] Test Detail", detail_only.to_string());
+
+    assert_eq!("[test:type 404] Not Found", type_status.to_string());
+    assert_eq!("[test:type] Test Title", type_title.to_string());
+    assert_eq!("[test:type] Test Detail", type_detail.to_string());
+    assert_eq!("[about:blank 404] Test Title", status_title.to_string());
+    assert_eq!(
+        "[about:blank 404] Not Found: Test Detail",
+        status_detail.to_string()
+    );
+    assert_eq!(
+        "[about:blank] Test Title: Test Detail",
+        title_detail.to_string()
+    );
+
+    assert_eq!("[test:type 404] Test Title", type_status_title.to_string());
+    assert_eq!(
+        "[test:type 404] Not Found: Test Detail",
+        type_status_detail.to_string()
+    );
+    assert_eq!(
+        "[test:type] Test Title: Test Detail",
+        type_title_detail.to_string()
+    );
+    assert_eq!(
+        "[about:blank 404] Test Title: Test Detail",
+        status_title_detail.to_string()
+    );
+
+    assert_eq!("[test:type 404] Test Title: Test Detail", full.to_string());
+}
+
+#[cfg(feature = "serde")]
+#[test]
+fn serialize_empty() {
+    let empty = ProblemDetails::new();
+
+    let serialized = serde_json::to_value(empty).unwrap();
+
+    let expected = json!({});
+
+    assert_eq!(expected, serialized);
+}
+
+#[cfg(feature = "serde")]
+#[test]
+fn serialize_filled() {
+    #[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+    struct Extensions {
+        foo: String,
+        bar: u32,
+    }
+
+    let details = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_status(StatusCode::INTERNAL_SERVER_ERROR)
+        .with_title("Test Title")
+        .with_detail("Test Detail")
+        .with_instance(Uri::from_static("test:instance"))
+        .with_extensions(Extensions {
+            foo: "Foo".to_string(),
+            bar: 42,
+        });
+
+    let serialized = serde_json::to_value(details).unwrap();
+
+    let expected = json!({
+        "type": "test:type",
+        "status": 500,
+        "title": "Test Title",
+        "detail": "Test Detail",
+        "instance": "test:instance",
+        "foo": "Foo",
+        "bar": 42
+    });
+
+    assert_eq!(expected, serialized);
+}
+
+#[cfg(feature = "serde")]
+#[test]
+fn deserialize_empty() {
+    let empty = json!({});
+
+    let deserialized = serde_json::from_value(empty).unwrap();
+
+    let expected = ProblemDetails::new();
+
+    assert_eq!(expected, deserialized);
+}
+
+#[cfg(feature = "serde")]
+#[test]
+fn deserialize_filled() {
+    #[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+    struct Extensions {
+        foo: String,
+        bar: u32,
+    }
+
+    let filled = json!({
+        "type": "test:type",
+        "status": 500,
+        "title": "Test Title",
+        "detail": "Test Detail",
+        "instance": "test:instance",
+        "foo": "Foo",
+        "bar": 42
+    });
+
+    let deserialized: ProblemDetails<Extensions> = serde_json::from_value(filled).unwrap();
+
+    let expected = ProblemDetails::new()
+        .with_type(Uri::from_static("test:type"))
+        .with_status(StatusCode::INTERNAL_SERVER_ERROR)
+        .with_title("Test Title")
+        .with_detail("Test Detail")
+        .with_instance(Uri::from_static("test:instance"))
+        .with_extensions(Extensions {
+            foo: "Foo".to_string(),
+            bar: 42,
+        });
+
+    assert_eq!(expected, deserialized);
+}
